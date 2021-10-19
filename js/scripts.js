66a68ba393065d7e22b913f071a18e8df04629a0
Caelian-A <92312529+Caelian-A@users.noreply.github.com>
2021-10-19 12:28:44 +0100

    Defined the array pokemonList, added 5 Pokémon, removed old code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-let favoriteFood = 'Lasagne';
-document.write(favoriteFood)
+let pokemonList = [];
+pokemonList = [
+  {name: 'Eevee', height: 0.3, type: ['normal']},
+  {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']},
+  {name: 'Charmander', height: 0.6, type: ['fire']},
+  {name: 'Squirtle', height: 0.5, type: ['water']},
+  {name: 'Pidgey', height: 0.3, type: ['flying', 'normal']}
+];
