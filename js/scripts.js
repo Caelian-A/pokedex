21910fb436ef22a626d9df077ff8c5cb8c9591ac
Caelian-A <92312529+Caelian-A@users.noreply.github.com>
2021-10-20 12:42:48 +0100

    index.html now lists each Pokémon in the array and their heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemonList = [];
+//Defining the array
 pokemonList = [
   {name: 'Eevee', height: 0.3, type: ['normal']},
   {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']},
@@ -6,3 +6,10 @@ pokemonList = [
   {name: 'Squirtle', height: 0.5, type: ['water']},
   {name: 'Pidgey', height: 0.3, type: ['flying', 'normal']}
 ];
+//Listing the name and height of each pokemon in the array.
+for (let i = 0; i < pokemonList.length; i++){
+  document.write(pokemonList[i].name + " - Height: " + pokemonList[i].height +"m");
+// Conditional to deteremine which Pokemon have a height greater than 0.6m. using <br> temporarily until i figure out how to make it a list!
+  if (pokemonList[i].height > 0.6) document.write("  - Wow, thats tall!" + "<br>");
+  else document.write("<br>");
+}
