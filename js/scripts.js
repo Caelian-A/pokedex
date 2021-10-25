0ab37c78e5b6eb82e42a584ad376779f2994c251
Caelian-A <92312529+Caelian-A@users.noreply.github.com>
2021-10-25 12:13:51 +0100

    Updated to include IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,40 @@
-//Defining the array
-pokemonList = [
-  {name: 'Eevee', height: 0.3, type: ['normal']},
-  {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']},
-  {name: 'Charmander', height: 0.6, type: ['fire']},
-  {name: 'Squirtle', height: 0.5, type: ['water']},
-  {name: 'Pidgey', height: 0.3, type: ['flying', 'normal']}
+//Start of IIFE
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Eevee', height: 0.3, type: ['normal']},
+    {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']},
+    {name: 'Charmander', height: 0.6, type: ['fire']},
+    {name: 'Squirtle', height: 0.5, type: ['water']},
+    {name: 'Pidgey', height: 0.3, type: ['flying', 'normal']}
 ];
-document.write('<ul>')
-for (let i = 0; i < pokemonList.length; i++){
-  document.write('<li>')
-  document.write(pokemonList[i].name + " - Height: " + pokemonList[i].height +"m");
-  if (pokemonList[i].height > 0.6) document.write("  - Wow, thats tall!");
+
+  //returns the pokemonList array
+  function getAll() {
+    return pokemonList;
+};
+
+  //adds new data to the pokemonList array
+  function add(pokemonData) {
+    pokemonList.push(pokemonData);
+};
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+
+  //End of IIFE
+})();
+  document.write('<ul>');
+//define data to new variables using getAll()
+pokemonRepository.getAll().forEach(function(pokemonData){
+  let pokemonName = pokemonData.name
+  let pokemonHeight = pokemonData.height
+  let PokemonType = pokemonData.type
+  //Write to the document.
+  document.write('<li>');
+  document.write(pokemonName + " - Height: " + pokemonHeight +"m");
+  if (pokemonHeight > 0.6) document.write("  - Wow, thats tall!");
   document.write("</li>");
-}
+});
 document.write("</ul>");
